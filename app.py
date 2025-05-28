d4238ea4508535181a648ee79c4e53fdb4200c6e
Bryan <bjakes45@gmail.com>
2025-05-28 13:53:52 -0700

    Fix for Deploy

--- app.py
+++ app.py
@@ -184,8 +184,8 @@ def initialize_database():
       db.session.add(user)
       db.session.commit()
 
-    if User.query.count() <= 1:
-      seed_users_with_interactions()
+    #if User.query.count() <= 1:
+     # seed_users_with_interactions()
 
     if Deal.query.count() <= 100:
       seed_deals()
